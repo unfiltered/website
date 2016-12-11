75b49da2a22291845a924223e4890c0a2818f4b6
xuwei-k <6b656e6a69@gmail.com>
2016-12-11 09:52:32 +0900

    specify SBT_GHPAGES_COMMIT_MESSAGE

--- publish.sh
+++ publish.sh
@@ -7,5 +7,7 @@ if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; 
   chmod 600 ~/.ssh/deploy.key
   git config --global user.email "6b656e6a69+unfiltered@gmail.com"
   git config --global user.name "unfiltered-bot"
+  # https://github.com/sbt/sbt-ghpages/commit/cbedd8edb8
+  export SBT_GHPAGES_COMMIT_MESSAGE="auto commit on travis https://github.com/unfiltered/website/commit/${TRAVIS_COMMIT} ${TRAVIS_JOB_NUMBER}"
   sbt ghpagesPushSite
 fi
