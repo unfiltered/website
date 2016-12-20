03a564c06d73f0636a22f41dea2296291aceed68
xuwei-k <6b656e6a69@gmail.com>
2016-12-20 23:22:06 +0900

    check TRAVIS_REPO_SLUG env

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_REPO_SLUG}" == "unfiltered/website" ]]; then
   echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
   openssl aes-256-cbc -k "$SERVER_KEY" -in deploy_key.enc -d -a -out deploy.key
   cp deploy.key ~/.ssh/
